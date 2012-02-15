ec8791c39a2553e2ec539575d98d7ab4b720e448
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-02-15 04:03:18 +0400

    initial version

---
+++ lib/Ubic/Service/InitScriptWrapper.pm
@@ -0,0 +1,115 @@
+package Ubic::Service::InitScriptWrapper;
+
+# ABSTRACT: represent any /etc/init.d/ script as ubic service
+
+=head1 SYNOPSIS
+
+    # in /etc/ubic/service/my-nginx
+    use Ubic::Service::InitScriptWrapper;
+    # map /etc/init.d/nginx to 'my-nginx' ubic service
+    Ubic::Service::InitScriptWrapper->new('nginx');
+
+=head1 DESCRIPTION
+
+This module lets you turn any LSB-compliant init script into ubic service.
+
+Note that this is completely different from L<Ubic::Run>. C<Ubic::Run> lets you
+turn ubic service to init script. This module does the reverse thing.
+
+=head1 WHY?
+
+There are several reasons why this module can be useful.
+
+First, it allows you to use all ubic features (watchdog, pretty CLI interface,
+persistent service states) without changing any of your code.
+
+Second, some daemons don't provide the way not to detach them from a terminal.
+Classic init scripts usually use C<start-stop-daemon> to start these daemons.
+C<Ubic::Daemon> and C<Ubic::Service::SimpleDaemon> can't be used to start these
+processes.
+You could write C<system('start-stop-daemon ...')> in your ubic service code,
+but if you already got a working init script, why bother?
+
+=head1 CAVEATS
+
+=over
+
+=item *
+
+Init script must conform to LSB specification
+(L<http://refspecs.linuxbase.org/LSB_4.0.0/LSB-Core-generic/LSB-Core-generic/iniscrptact.html>).
+
+At the very least, it must exit with 0 code on successful start and stop and
+status commands, and with non-zero code on unsuccessful status command.
+
+=item *
+
+This module doesn't distinguish C<broken> and C<not running> states yet.
+It interprets any non-zero status code as C<not running>.
+
+=back
+
+=head1 CONSTRUCTOR
+
+=over
+
+=item B<new($init_script_name)>
+
+If C<$init_script_name> contains C</>, it will be interpreted as the filename.
+
+Otherwise, C</etc/init.d/$init_script_name> will be used.
+
+=cut
+
+use strict;
+use warnings;
+
+use parent qw(Ubic::Service::Skeleton);
+use autodie qw(:all);
+use IPC::System::Simple; # force system() support for autodie
+
+sub new {
+    my $class = shift;
+    my ($init) = @_;
+    die "Invalid parameters" unless @_ == 1;
+
+    if ($init !~ m{/}) {
+        $init = "/etc/init.d/$init";
+    }
+
+    unless (-e $init) {
+        die "Init script $init not found";
+    }
+
+    return bless { init => $init } => $class;
+}
+
+sub start_impl {
+    my $self = shift;
+    system("$self->{init} start >/dev/null");
+}
+
+sub stop_impl {
+    my $self = shift;
+    system("$self->{init} stop >/dev/null");
+}
+
+sub reload {
+    my $self = shift;
+    system("$self->{init} reload >/dev/null");
+    return 'reloaded'; # we expect system() to fail if reload is not implemented
+}
+
+sub status_impl {
+    my $self = shift;
+    no autodie;
+    my $code = system("$self->{init} status >/dev/null");
+    if ($code == 0) {
+        return 'running';
+    }
+    else {
+        return 'not running'; # TODO - distinguish 'not running' and 'broken'
+    }
+}
+
+1;
